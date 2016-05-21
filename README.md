afc78c2272ab9be61df498b2f60a57a0332fbb18
guoyff <guoyff@yonyou.com>
2016-05-21 15:49:51 +0800

    download gulp plugin & config

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # iUAP-Design
 
 iUAP-Design 是面向互联网企业的 UI 设计语言，具有开放自由、易学易用、美观大气等特点，并且提供从产品界面设计到前端开发的完整开发生态，可以大大提升设计和开发的效率。
+
+## 开发
+
+```
+$ npm install
+```
+
+`特别说明：windows上装gulp-sass会失败，因为gulp-sass依赖了node-sass，这个包有个二进制文件，一般需要安装成功有几种方法：1.用vpn翻墙；2.安装vs2015，并且配置msvs_version=2105。`
+
+调试：
+
+```
+$ npm run dev
+```
+
+## 博客
+
+```
+$ cd docs && jekyll serve
+# localhost://4000查看效果
+```
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,9 +8,11 @@ import concat from 'gulp-concat';
 import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
 import minifycss from 'gulp-minify-css';
+import sourcemaps from 'gulp-sourcemaps';
+import babel from 'gulp-babel';
 
 gulp.task('sass', () => {
-  return gulp.src('./scss/**/*.scss')
+  return gulp.src(['./scss/u.scss', './scss/u.extend.scss'])
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(autoprefixer({
         browsers: ['last 2 versions'],
@@ -19,8 +21,28 @@ gulp.task('sass', () => {
     .pipe(gulp.dest('./dist/css'));
 });
 
-gulp.task('sass:watch', () => {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task("es2015", function () {
+  return gulp.src("js/src/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat("u.js"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("dist/js"));
 });
 
-gulp.task('default', ['sass:watch'])
+gulp.task('serve', function() {
+    // static server
+    browserSync({
+        files: ['scss/**/*.scss', 'src/**.js', 'dist', 'plugins'],
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    // watch task
+    gulp.watch('./scss/**/*.scss', ['sass']);
+    gulp.watch('./js/src/*.js', ['es2015']);
+
+});
+
+gulp.task('default', ['sass', 'es2015', 'serve'])
