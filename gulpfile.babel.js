9e68f7e6d9e454c9fc0d0428c1c496c30f448ff7
guoyff <guoyff@yonyou.com>
2016-05-27 10:43:50 +0800

    saave

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -23,10 +23,6 @@ const UISassSrcPath = [
   'src/ui-extend/u-extend.scss'
 ]
 
-const gridSassSrcPath = 'src/ui/grid/grid.css'
-
-const treeSassSrcPath = 'src/ui/tree/tree.css'
-
 const UISrcPath = [
   // core
   'src/core/core/core.js',
@@ -84,62 +80,6 @@ const UISrcPath = [
   // polyfill
   'src/polyfill/core.js',
   'src/polyfill/JsExtensions.js'
-]
-
-const gridSrcPath = [
-  'src/ui/grid/gridComp.js',
-
-  'src/ui/grid/ColumnMenu.js',
-  'src/ui/grid/Drag.js',
-  'src/ui/grid/Edit.js',
-  'src/ui/grid/EditForm.js',
-  'src/ui/grid/Fixed.js',
-  'src/ui/grid/FormShow.js',
-  'src/ui/grid/HeaderLevel.js',
-  'src/ui/grid/OverWidthHidden.js',
-  'src/ui/grid/Sort.js',
-  'src/ui/grid/SumRow.js',
-  'src/ui/grid/Swap.js',
-  'src/ui/grid/Tree.js',
-  'src/ui/grid/grid-comp-adp.js'
-]
-
-const treeSrcPath = [
-  'src/ui/tree/treeComp.js',
-  'src/ui/tree/tree-comp-adp.js'
-]
-
-const modelSrcPath = [
-  'src/model/core/core.js',
-  'src/model/core/app.js',
-  'src/model/dataTable/dataTable.js',
-  'src/model/comp-adp/baseAdapter.js',
-  'src/model/comp-adp/mixins/enableMixin.js',
-  'src/model/comp-adp/mixins/requiredMixin.js',
-  'src/model/comp-adp/mixins/validateMixin.js',
-  'src/model/comp-adp/mixins/valueMixin.js',
-  'src/model/comp-adp/integer.js',
-  'src/model/comp-adp/float.js',
-  'src/model/comp-adp/currency.js',
-  'src/model/comp-adp/percent.js',
-  'src/model/comp-adp/string.js',
-  'src/model/comp-adp/textarea.js',
-  'src/model/comp-adp/textfield.js',
-  'src/model/comp-adp/checkbox.js',
-  'src/model/comp-adp/switch.js',
-  'src/model/comp-adp/combobox.js',
-  'src/model/comp-adp/radio.js',
-  'src/model/comp-adp/native-radio.js',
-  'src/model/comp-adp/native-checkbox.js',
-  'src/model/comp-adp/pagination.js',
-  'src/model/comp-adp/datetime.js',
-  'src/model/comp-adp/time.js',
-  'src/model/comp-adp/yearmonth.js',
-  'src/model/comp-adp/year.js',
-  'src/model/comp-adp/month.js',
-  'src/model/comp-adp/progress.js',
-  'src/model/comp-adp/url.js',
-  'src/model/comp-adp/password.js',
 ]
 
 const AUTOPREFIXER_BROWSERS = [
@@ -219,105 +159,6 @@ gulp.task('ui-js-dist', function(){
 });
 
 /**
- * grid 控件
- * @param  {[type]} 'grid'    [description]
- * @param  {[type]} function( [description]
- * @return {[type]}           [description]
- */
-gulp.task('grid', () => {
-     gulp.src( gridSrcPath )
-        .pipe(concat('u-grid.js'))
-        .on('error', errHandle)
-        .pipe(gulp.dest('dist/js'));
-
-    gulp.src( gridSassSrcPath )
-        .pipe(gulp.dest('dist/css'))
-});
-
-/**
- * grid 控件生产环境压缩
- * @param  {[type]} 'grid:dist' [description]
- * @param  {[type]} function(   [description]
- * @return {[type]}             [description]
- */
-gulp.task('grid:dist', () => {
-     gulp.src( gridSrcPath )
-        .pipe(concat('u-grid.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-
-    gulp.src( gridSassSrcPath )
-        .pipe(gulp.dest('dist/css'))
-});
-
-/**
- *tree 控件
- * @param  {[type]} 'grid'    [description]
- * @param  {[type]} function( [description]
- * @return {[type]}           [description]
- */
-gulp.task('tree', () => {
-     gulp.src( treeSrcPath )
-        .pipe(concat('u-tree.js'))
-        .on('error', errHandle)
-        .pipe(gulp.dest('dist/js'));
-
-    gulp.src( treeSassSrcPath )
-        .pipe(gulp.dest('dist/css'))
-});
-
-/**
- * tree 控件生成环境版
- * @param  {[type]} 'tree:dist' [description]
- * @param  {[type]} function(   [description]
- * @return {[type]}             [description]
- */
-gulp.task('tree:dist', () => {
-   gulp.src( treeSrcPath )
-      .pipe(concat('u-tree.min.js'))
-      .pipe(uglify())
-      .pipe(gulp.dest('dist/js'));
-
-  gulp.src( treeSassSrcPath )
-      .pipe(gulp.dest('dist/css'))
-});
-
-/**
- * datatable 逻辑合并
- * @param  {[type]} 'datatable' [description]
- * @param  {[type]} (           [description]
- * @return {[type]}             [description]
- */
-gulp.task('model', () => {
-   return gulp.src( modelSrcPath )
-    .pipe(concat('u-model.js'))
-    .on('error', errHandle)
-    .pipe(gulp.dest('dist/js'));
-});
-
-/**
- * 压缩版 datatable
- * @param  {[type]} 'datatable:dist' [description]
- * @param  {[type]} (                [description]
- * @return {[type]}                  [description]
- */
-gulp.task('model:dist', () => {
-   return gulp.src( modelSrcPath )
-    .pipe(concat('u-model.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('aio', ['clean', 'es-ui', 'grid', 'tree', 'model'], () => {
-   return gulp.src('dist/js/*.js')
-    .pipe(concat('u.js'))
-    .pipe(gulp.dest('dist/js'))
-    .pipe(uglify())
-    .pipe(rename('u.min.js'))
-    .pipe(gulp.dest('dist/js'));
-});
-
-/**
  * 搬运图标字体，直接复制拷贝
  * @param  {[type]} 'font' [description]
  * @param  {[type]} (      [description]
@@ -347,7 +188,6 @@ gulp.task('serve', () => {
 
     gulp.watch('./src/**/**/*.scss', ['sass']);
     gulp.watch(['./src/core/**/*.js', './src/ui/**/*.js'], ['es-ui']);
-    gulp.watch('./src/model/**/*.js', ['model']);
 
 });
 
@@ -363,16 +203,5 @@ gulp.task('clean', () => {
     .on('error', errHandle);
 });
 
-gulp.task('dev', [
-  'font', 'sass-ui', 'es-ui', 'grid', 'tree',
-  'model', 'serve',
-])
-
-gulp.task('prod', [
-  'font', 'ui-js-dist', 'sass-ui', 'grid:dist', 'tree:dist',
-  'model:dist', 'aio'
-])
-
-gulp.task('prod:ui', [
-  'font', 'ui-js-dist', 'sass-ui', 'grid:dist', 'tree:dist'
-])
+gulp.task('dev', ['font', 'sass-ui', 'es-ui', 'serve'])
+gulp.task('prod', ['font', 'ui-js-dist', 'sass-ui'])
