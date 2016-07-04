2fa386ace8d7b1792625d05763b6dbe9a1f2e11d
wanghaoo <wanghao@yonyou.com>
2016-07-04 10:41:00 +0800

    提交到master上的文件迁移到release上

--- snippets/examples/pagination/4-sizePa/widget.js
+++ snippets/examples/pagination/4-sizePa/widget.js
@@ -1,9 +1,9 @@
 var paginationLg = document.getElementById("paginationLg");
-var comp = new u.pagination({el:paginationLg,jumppage:true});
-comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200,showState:false});
+var comp = new u.pagination({el:paginationLg,showState:false });
+comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200});
 var paginationDefault = document.getElementById("paginationDefault");
-var comp = new u.pagination({el:paginationDefault,jumppage:true});
-comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200,showState:false});
+var comp = new u.pagination({el:paginationDefault,showState:false });
+comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200});
 var paginationSm = document.getElementById("paginationSm");
-var comp = new u.pagination({el:paginationSm,jumppage:true});
-comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200,showState:false});
+var comp = new u.pagination({el:paginationSm,showState:false });
+comp.update({totalPages: 100,pageSize:20,currentPage:1,totalCount:200});
