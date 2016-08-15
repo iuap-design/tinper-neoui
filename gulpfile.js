313dd56ce665cdc91d08d27c97da6dfe1f722426
onvno <leewei2020@126.com>
2016-08-15 14:10:18 +0800

    调整输出配置&增加datetimepicker样式

--- gulpfile.js
+++ gulpfile.js
@@ -14,68 +14,6 @@ var util = require('gulp-util');
 var stripCssComments = require('gulp-strip-css-comments');
 
 var version = require('./version.js');
-/**
- * SASS 源文件索引
- * @type {Array}
- */
-var UISassSrcPath = [
-    'scss/u.scss',
-    'scss/u-extend.scss',
-]
-
-var UISrcPath = [
-    'js/core/core.js',
-    'js/core/event.js',
-    'js/utilities/jsExtensions.js',
-    'js/core/ajax.js',
-    'js/core/base.js',
-    'js/core/compMgr.js',
-    'js/utilities/i18n.js',
-    'js/utilities/rsautils.js',
-    'js/utilities/masker.js',
-    'js/utilities/formater.js',
-    'js/utilities/dateUtils.js',
-    'js/utilities/dataRender.js',
-    'js/utilities/hotKeys.js',
-    'js/core/BaseComponent.js',
-    'js/extend/ripple.js',
-    'js/ui.button.js',
-    'js/layout.nav.js',
-    'js/ui.navmenu.js',
-    'js/ui.textfield.js',
-    'js/extend/ui.menu.js',
-    'js/layout.md.js',
-    'js/ui.tabs.js',
-    'js/ui.checkbox.js',
-    'js/ui.radio.js',
-    'js/ui.switch.js',
-    'js/extend/ui.loading.js',
-    'js/extend/ui.loader.js',
-    'js/ui.progress.js',
-    'js/ui.message.js',
-    'js/extend/messageDialog.js',
-    'js/extend/confirmDialog.js',
-    'js/extend/threeBtnDialog.js',
-    'js/dialog.js',
-    'js/extend/combobox.js',
-    'js/extend/ui.multilang.js',
-    'js/extend/autocomplete.js',
-    'js/extend/ui.combo.js',
-    'js/data-table.js',
-    'js/extend/ui.pagination.js',
-    'js/ui.tooltip.js',
-    'js/validate.js',
-    'js/extend/ui.refer.js',
-    'js/extend/slidePanel.js',
-    'js/core/end.js'
-]
-
-var polyPath = [
-    'vendor/polyfill/core.js',
-    'vendor/polyfill/JsExtensions.js',
-    'vendor/polyfill/respond.js'
-]
-
 var AUTOPREFIXER_BROWSERS = [
     'ie >= 11',
     'edge >= 20',
@@ -84,7 +22,7 @@ var AUTOPREFIXER_BROWSERS = [
     'safari >= 8',
     'opera >= 35',
     'ios >= 8'
-]
+];
 
 /**
  * 公共错误处理函数
@@ -104,91 +42,7 @@ var errHandle = function ( err ) {
     util.log( err );
 
     this.end();
-}
-
-/**
- * 编译 SASS 文件，并自动添加浏览器前缀
- * @param  {[type]} 'sass-ui' [description]
- * @param  {[type]} (         [description]
- * @return {[type]}           [description]
- */
-
-gulp.task('sass-ui-u-init', function () {
-
-    return gulp.src(['scss/u.scss', 'scss/u-extend.scss'])
-        .pipe(concat('u.scss'))
-        .pipe(sass().on('error', errHandle))
-        .pipe(base64().on('error',errHandle))
-        .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(minifycss())
-        .pipe(rename('u.min.css'))
-        .pipe(gulp.dest('dist/css'));
-});
-
-gulp.task('sass-ui-u-extend-init', function(){
-    return gulp.src('scss/u-extend.scss')
-        .pipe(sass().on('error',errHandle))
-        .pipe(base64().on('error',errHandle))
-        .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(minifycss())
-        .pipe(rename('u-extend.min.css'))
-        .pipe(gulp.dest('dist/css'));
-})
-
-gulp.task('sass-ui', ['sass-ui-u-init', 'sass-ui-u-extend-init'], function(){
-    version.init([
-            'dist/css/u.css',
-            'dist/css/u.min.css',
-            'dist/css/u-extend.css',
-            'dist/css/u-extend.min.css'
-        ]);
-})
-
-
-/**
- * 编译并合并 UI 相关的 JS 文件
- * 用于开发环境，并支持 ES6/7 语法，可产出map文件
- * @param  {[type]} "es-ui" [description]
- * @param  {[type]} (       [description]
- * @return {[type]}         [description]
- */
-gulp.task("es-ui-init", function () {
-    return gulp.src( UISrcPath )
-            .pipe(concat('u-ui.js'))
-            .pipe(gulp.dest('./dist/js'))
-            .pipe(uglify().on('error', errHandle))
-            .pipe(rename('u-ui.min.js'))
-            .pipe(gulp.dest('./dist/js'));
-});
-
-gulp.task('es-ui', ['es-ui-init'], function(){
-     version.init([
-            'dist/js/u-ui.js',
-            'dist/js/u-ui.min.js',
-        ]);
-})
-
-
-gulp.task("polyfill-init", function () {
-    return gulp.src(polyPath)
-            .pipe(concat("neoui-polyfill.js"))
-            .pipe(gulp.dest("./dist/js"))
-            .pipe(uglify())
-            .on('error', errHandle)
-            .pipe(concat("neoui-polyfill.min.js"))
-            .pipe(gulp.dest("./dist/js"));
-});
-
-
-gulp.task('polyfill', ['polyfill-init'], function(){
-     version.init([
-            'dist/js/neoui-polyfill.js',
-            'dist/js/neoui-polyfill.min.js',
-        ]);
-})
-
+};
 
 /**
  * 搬运图标字体，直接复制拷贝
@@ -201,7 +55,6 @@ gulp.task('font', function () {
       .pipe(gulp.dest('./dist/fonts/font-awesome'));
   return gulp.src('./fonts/**')
          .pipe(gulp.dest('./dist/fonts'));
-
 });
 
 /**
@@ -246,6 +99,7 @@ gulp.task('clean', function () {
 
 /**
  * 插件CSS 构建
+ * 暂时不输出
  */
 gulp.task('vsass', function(){
     gulp.src('./scss/**/*')
@@ -263,51 +117,18 @@ gulp.task('vcss', ['vsass'], function(){
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./v1/lib/css'));
 });
-
-
-gulp.task('dev', ['image', 'font', 'sass-ui', 'es-ui', 'polyfill', 'serve'])
-gulp.task('dist', ['image', 'font', 'sass-ui', 'es-ui', 'polyfill'])
-gulp.task('vbuild',['vcss']);
-
-
-
-/**
- * [执行重构后dist/css目录输出]
- * 产出map文件,暂时不输出
- */
-gulp.task('mapcss', function(){
-    gulp.src('./scss/neoui.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./dist/css'))
-        .pipe(sourcemaps.init())
-        .pipe(minifycss())
-        .pipe(rename({
-            suffix:'.min'
-        }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist/css'));
-});
-gulp.task('mapcssplugin',function(){
-    gulp.src('./scss/ui/*.*')
-        .pipe(sass())
-        .pipe(gulp.dest('./dist/css/plugin'))
-        .pipe(sourcemaps.init())
-        .pipe(minifycss())
-        .pipe(rename({
-            suffix:'.min'
-        }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist/css/plugin'));
-});
+// gulp.task('dev', ['image', 'font', 'sass-ui', 'es-ui', 'polyfill', 'serve'])
 
 /**
  * [执行重构后dist/css目录输出]
  * 不产出map文件
  */
+// 输出整体css文件dist/css/neoui.css
 gulp.task('buildcss', function(){
     gulp.src('./scss/neoui.scss')
         .pipe(sass())
         .pipe(base64())
+        .pipe(autoprefixer(AUTOPREFIXER_BROWSERS))
         .pipe(stripCssComments())
         .pipe(gulp.dest('./dist/css'))
         .pipe(minifycss())
@@ -316,6 +137,7 @@ gulp.task('buildcss', function(){
         }))
         .pipe(gulp.dest('./dist/css'));
 });
+// 逐一输出单个插件dist/css/plugins/
 gulp.task('buildcssplugin',function(){
     gulp.src('./scss/ui/*.*')
         .pipe(sass())
@@ -326,19 +148,18 @@ gulp.task('buildcssplugin',function(){
         }))
         .pipe(gulp.dest('./dist/css/plugin'));
 });
-
-gulp.task('distmap', ['mapcss', 'mapcssplugin']);
 gulp.task('distbuild', ['buildcss','buildcssplugin']);
 
 /**
  * 输出合并后的:
  * neoui.css
  * neoui.min.css
- * neoui-polyfill.js
- * neoui-polyfill.min.js
+ * neoui.js
+ * neoui.min.js
+ * neoui.js合并包含第三方插件
+ * 添加头部信息
  */
-gulp.task('vendorui', function(){
-    // gulp.src(['vendor/ui/slider.js','vendor/ui/wizard.js','dist/js/neoui.js'])
+gulp.task('buildjs', function(){
     gulp.src(['vendor/ui/*.js','dist/js/neoui.js'])
         .pipe(concat('neoui.js'))
         .pipe(gulp.dest('./dist/js'))
@@ -347,24 +168,19 @@ gulp.task('vendorui', function(){
         .pipe(gulp.dest('./dist/js'));
 
 });
-gulp.task('neo',['buildcss', 'vendorui'],function(){
+gulp.task('neo',['buildcss', 'buildjs'],function(){
     version.init([
         'dist/js/neoui.js',
         'dist/js/neoui.min.js'
     ]);    
 });
-gulp.task('cssvs', ['neo'], function(){
+gulp.task('neoui', ['neo'], function(){
     version.init([
         'dist/css/neoui.css',
         'dist/css/neoui.min.css'
     ]);    
 });
 
+gulp.task('dist', ['neoui','image','font']);
 
-/**
- * 输出单独插件
- * 输出目录:./dist/plugin
- * 输出plugin.css & plugin.min.css
- */
-gulp.task('neoplugin',['buildcssplugin']);
 
