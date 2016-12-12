166e28ebd8fb6ff8af7ccda92b8af6ba1dd95c9c
liuyk <liuyk@yonyou.com>
2016-12-12 09:19:17 +0800

    优化代码

--- js/neoui-clockpicker.js
+++ js/neoui-clockpicker.js
@@ -8,7 +8,7 @@ import {BaseComponent} from 'tinper-sparrow/js/BaseComponent';
 import {addClass, removeClass, hasClass, showPanelByEle, makeDOM, makeModal, getZIndex} from 'tinper-sparrow/js/dom';
 import {on, off} from 'tinper-sparrow/js/event';
 import {compMgr} from 'tinper-sparrow/js/compMgr'; 
-import {isMobile} from 'tinper-sparrow/js/env'; 
+import {isMobile, env} from 'tinper-sparrow/js/env'; 
 import {extend} from 'tinper-sparrow/js/extend'; 
 import {core} from 'tinper-sparrow/js/core'; 
 import {date} from 'tinper-sparrow/js/util/dateUtils'; 
