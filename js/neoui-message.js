993c03fea8331111334887b1dbed6f1b16c1f76e
yaoxinc <yaoxinc@yonyou.com>
2016-12-16 13:03:40 +0800

    修改message

--- js/neoui-message.js
+++ js/neoui-message.js
@@ -1,7 +1,7 @@
 /**
  * Module : neoui-message
  * Author : Kvkens(yueming@yonyou.com)
- * Date	  : 2016-08-02 19:40:59
+ * Date      : 2016-08-02 19:40:59
  */
 
 import {addClass,removeClass,makeDOM} from 'tinper-sparrow/js/dom';
@@ -10,62 +10,85 @@ import {on} from 'tinper-sparrow/js/event';
 
 var messageTemplate = '<div class="u-message"><span class="u-msg-close uf uf-removesymbol"></span>{msg}</div>';
 
-var showMessage = function(options) {
-	var msg, position, width, height, showSeconds, msgType, template;
-	if(typeof options === 'string') {
-		options = {
-			msg: options
-		};
-	}
-	msg = options['msg'] || "";
-	position = options['position'] || "bottom-right"; //center. top-left, top-center, top-right, bottom-left, bottom-center, bottom-right,
-	//TODO 后面改规则：没设宽高时，自适应
-	width = options['width'] || "";
-	// height = options['height'] || "100px";
-	msgType = options['msgType'] || 'info';
-	//默认为当用户输入的时间，当用户输入的时间为false并且msgType=='info'时，默认显示时间为2s
-	showSeconds = parseInt(options['showSeconds']) || (msgType == 'info' ? 2 : 0);
+var showMessage = function (options) {
+    var msg, position, width, height, showSeconds, msgType, template;
+    //新增深色
+    var darkType;
+    if (typeof options === 'string') {
+        options = {
+            msg: options
+        };
+    }
+    msg = options['msg'] || "";
+    position = options['position'] || "bottom"; //center. top-left, top-center, top-right, bottom-left, bottom-center, bottom-right,
+    //TODO 后面改规则：没设宽高时，自适应
+    width = options['width'] || "";
+    // height = options['height'] || "100px";
+    msgType = options['msgType'] || 'info';
+    //默认为当用户输入的时间，当用户输入的时间为false并且msgType=='info'时，默认显示时间为2s
+    showSeconds = parseInt(options['showSeconds']) || (msgType == 'info' ? 2 : 0);
 
-	template = options['template'] || messageTemplate;
+    darkType = options['darkType'] || "";
 
-	template = template.replace('{msg}', msg);
-	var msgDom = makeDOM(template);
-	addClass(msgDom, 'u-mes' + msgType);
-	msgDom.style.width = width;
-	// msgDom.style.height = height;
-	// msgDom.style.lineHeight = height;
-	if(position == 'bottom-right') {
-		msgDom.style.bottom = '10px';
-	}
+    template = options['template'] || messageTemplate;
 
-	if(position == 'center') {
-		msgDom.style.bottom = '50%';
-		msgDom.style.transform = 'translateY(50%)';
+    template = template.replace('{msg}', msg);
+    var msgDom = makeDOM(template);
+    addClass(msgDom, 'u-mes' + msgType);
+
+    if (!darkType == "") {
+        addClass(msgDom, darkType);
+    }
+
+    msgDom.style.width = width;
+    // msgDom.style.height = height;
+    // msgDom.style.lineHeight = height;
+    if (position == 'bottom' || position == 'top' || position == 'center') {
+			//msgDom.style.bottom = '10px';
+			addClass(msgDom, 'u-mes-' + position);
 	}
-	var closeBtn = msgDom.querySelector('.u-msg-close');
-	//new Button({el:closeBtn});
-	var closeFun = function(){
-    	removeClass(msgDom,"active")
-        setTimeout(function(){
-            try{
+	
+	if (position == 'topleft' || position == 'bottomleft') {
+		if(width == ""){
+			    msgDom.style.right = '2.4rem';
+			    addClass(msgDom, 'u-mes-' + position);
+		}else{
+			    addClass(msgDom, 'u-mes-' + position);
+			}	
+	}
+	if (position == 'topright' || position == 'bottomright') {
+		if(width == ""){
+				msgDom.style.left = '2.4rem';
+				addClass(msgDom, 'u-mes-' + position);
+		}else{
+				addClass(msgDom, 'u-mes-' + position);
+			}
+		
+	}	
+    var closeBtn = msgDom.querySelector('.u-msg-close');
+    //new Button({el:closeBtn});
+    var closeFun = function () {
+        removeClass(msgDom, "active")
+        setTimeout(function () {
+            try {
                 document.body.removeChild(msgDom);
-            }catch(e){
- 
+            } catch (e) {
+
             }
-        },500) 
+        }, 500)
     }
     on(closeBtn, 'click', closeFun);
     document.body.appendChild(msgDom);
-   
-    if (showSeconds > 0 ){
-        setTimeout(function(){
-        	closeFun();
-        },showSeconds* 1000)
+
+    if (showSeconds > 0) {
+        setTimeout(function () {
+            closeFun();
+        }, showSeconds * 1000)
     }
 
-	setTimeout(function() {
-		addClass(msgDom, "active")
-	}, showSeconds * 1)
+    setTimeout(function () {
+        addClass(msgDom, "active")
+    }, showSeconds * 1)
 
 }
 
