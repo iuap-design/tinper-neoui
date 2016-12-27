a855858ef74756ef4b7cba2f2ea3ac4eb35d76d0
liuyk <liuyk@yonyou.com>
2016-12-27 10:13:17 +0800

    多语组件

--- js/neoui-multilang.js
+++ js/neoui-multilang.js
@@ -13,14 +13,11 @@ import {compMgr} from 'tinper-sparrow/js/compMgr';
 
 
 var Multilang = BaseComponent.extend({
-	DEFAULTS: {
-		dataSource: {},
-		onSelect: function() {}
-	},
 	init: function() {
 		var self = this;
 		var element = this.element;
 		this.options = extend({}, this.DEFAULTS, this.options);
+		this.field = this.options.field || 'name';
 		this.multinfo(this.options.multinfo);
 		this.addData(this.options.multidata);
 
@@ -29,7 +26,8 @@ var Multilang = BaseComponent.extend({
 Multilang.fn = Multilang.prototype;
 Multilang.fn.addData = function(val) {
 	var target = this.element,
-		tmparray, target_div = target.parentNode;
+		tmparray, 
+		target_div = target.parentNode;
 	if(val === null || typeof(val) === 'undefined' ){
 		tmparray = [];
 	} else if (typeof(val) == "object") {
@@ -37,7 +35,7 @@ Multilang.fn.addData = function(val) {
 	} else {
 		tmparray = val.split(",")
 	}
-	target_div.value = tmparray
+	target_div.value = tmparray;
 	each(tmparray, function(i, node) {
 		target_div.querySelectorAll(".m_context")[i].innerHTML = node
 	})
@@ -46,13 +44,9 @@ Multilang.fn.addData = function(val) {
 Multilang.fn.multinfo = function(sort) {
 
 	var target = this.element,
-		me = this,
+		self = this,
 		tmplabel = "",
-		close_menu = true,
-		tmpfield = "name";
-	// if(sort.lang_name) {
-	// 	tmpfield = sort.lang_name
-	// }
+		close_menu = false;
 	if(isArray(sort)) {
 
 		wrap(target, "<div class='multilang_body'><input class='lang_value' contenteditable='true'><span class='uf uf-caretdown lang_icon'><span class='m_icon'></span></span>")
@@ -60,14 +54,14 @@ Multilang.fn.multinfo = function(sort) {
 
 		each(sort, function(i, node) {
 			if(i) {
-				tmplabel += "<label attr='" + tmpfield + (i + 1) + "'><span class='m_context'></span><span class='m_icon'>" + node + "</span></label>"
+				tmplabel += "<label attr='" + self.field + (i + 1) + "'><span class='m_context'></span><span class='m_icon'>" + node + "</span></label>"
 			} else {
-				tmplabel += "<label attr='" + tmpfield + "'><span class='m_context'></span><span class='m_icon'>" + node + "</span></label>"
+				tmplabel += "<label attr='" + self.field + "'><span class='m_context'></span><span class='m_icon'>" + node + "</span></label>"
 			}
 		})
-		var target_div = target.parentNode
+		var target_div = target.parentNode;
 
-		target_div.insertAdjacentHTML("beforeEnd", "<div class='multilang_menu '>" + tmplabel + "</div>")
+		target_div.insertAdjacentHTML("beforeEnd", "<div class='multilang_menu '>" + tmplabel + "</div>");
 		var tmpIconv = target_div.querySelector(".lang_icon"),
 			target_menu = target_div.querySelector(".multilang_menu"),
 			target_labels = target_menu.querySelectorAll('label'),
@@ -88,56 +82,54 @@ Multilang.fn.multinfo = function(sort) {
 			close_menu = true;
 		})
 
-		on(tmpvaluebox, "blur", function() {
-			//this//
-			//target_box = me.fixtarget(target_input),
-			//target_div = target_input.parents(".multilang_body"),
-			target = this
-			tmpkey = target.className.split(" ")[2],
+		on(tmpvaluebox, "blur", function(e) {
+			var target_input = $(this),
+				target_div = target_input.parents(".multilang_body"),
+				target = e.target,
+				tmpkey = target.className.split(" ")[2],
 				tmptext = target.value;
 
 			if(hasClass(target, "ready_change")) {
-				me.changeData(target_div, tmpkey, tmptext);
+				self.changeData(target_div[0], tmpkey, tmptext);
 			}
-			if(close_menu) {
-				css(target_menu, "display", "none")
-			}
+			// if(close_menu) {
+			// 	css(target_menu, "display", "none")
+			// }
 
 		})
 
 		target_labels.forEach(function(ele){
 			on(ele, "click", function() {
 				var target_label = this,
-					tmpfield = target_label.getAttribute("attr"),
+					tempField = target_label.getAttribute("attr"),
 					tmptext = target_label.querySelector(".m_context").innerHTML,
 					tmpicon = target_label.querySelector(".m_icon").cloneNode(true);
 
-				tmpvaluebox.setAttribute("class", "ready_change lang_value " + tmpfield)
+				tmpvaluebox.setAttribute("class", "ready_change lang_value " + tempField)
 				tmpvaluebox.value = tmptext
 				tmpvaluebox.focus();
 				var tmpicom = target_div.querySelector(".lang_icon"),
-					oldicon = target_div.querySelector(".m_icon")
-				removeClass(tmpicom, "uf-caretdown")
-				tmpicom.replaceChild(tmpicon, oldicon)
+					oldicon = target_div.querySelector(".m_icon");
+				removeClass(tmpicom, "uf-caretdown");
+				tmpicom.replaceChild(tmpicon, oldicon);
 			})
 		})
-
 
 	} else {
 		console.error('Not object')
 	}
 }
 Multilang.fn.changeData = function(target_div, field, text) {
-	var tmpdata = target_div.value;
-	tmplabel = target_div.querySelector("label[attr='" + field + "']");
-	tmpcontext = tmplabel.querySelector(".m_context");
-	tmpcontext.innerHTML = text
-	tmpcontext.value = text
+	var tmpdata = target_div.value,
+		tmplabel = target_div.querySelector("label[attr='" + field + "']"),
+		tmpcontext = tmplabel.querySelector(".m_context");
+	tmpcontext.innerHTML = text;
+	tmpcontext.value = text;
 	each(target_div.querySelectorAll(".m_context"), function(i, node) {
 		tmpdata[i] = node.innerHTML
 	})
 
-	trigger(this.element, 'change.u.multilang', {
+	this.trigger('change.u.multilang', {
 		newValue: text,
 		field: field
 	})
@@ -146,12 +138,23 @@ Multilang.fn.changeData = function(target_div, field, text) {
 Multilang.fn.getData = function() {
 	var target = $(multilang.target).next(".multilang_body")[0],
 		multilang_data = target.value;
+
 	return multilang_data;
 }
 
-Multilang.fn.setDataValue = function(value){
-	var target = this.element.parentNode.querySelector('.lang_value');
-	target.value = value;
+Multilang.fn.setDataValue = function(field, value){
+	var target_div = this.element.closest('.multilang_body'),
+		tmplabel = target_div.querySelector("label[attr='" + field + "']"),
+		tmpcontext = tmplabel.querySelector(".m_context");
+	tmpcontext.innerHTML = value;
+	tmpcontext.value = value;
+
+	var tmpdata = [];
+	each(this.element.closest('.multilang_body').querySelectorAll(".m_context"), function(i, node) {
+		tmpdata[i] = node.innerHTML
+	})
+	this.element.closest('.multilang_body').value = tmpdata;
+
 }
 
 compMgr.regComp({
