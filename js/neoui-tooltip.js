e632520d0071f36e3e1ccb8ab44fca92a5ad6848
huyueb <1062887235@qq.com>
2017-01-06 16:56:24 +0800

    fixed:tooltip优化，支持批量设置

--- js/neoui-tooltip.js
+++ js/neoui-tooltip.js
@@ -29,79 +29,86 @@ Tooltip.prototype = {
 		},
 		showFix: false
 	},
-	init: function(element, options) {
-		this.element = element
-		this.options = extend({}, this.defaults, options);
+	init: function init(element, options) {
+		var oThis = this;
+		this.options = (0, _extend.extend)({}, this.defaults, options);
 		this._viewport = this.options.viewport && document.querySelector(this.options.viewport.selector || this.options.viewport);
-
-		var triggers = this.options.trigger.split(' ')
-
-		for(var i = triggers.length; i--;) {
-			var trigger = triggers[i]
-			if(trigger == 'click') {
-				on(this.element, 'click', this.toggle.bind(this));
-			} else if(trigger != 'manual') {
-				var eventIn = trigger == 'hover' ? 'mouseenter' : 'focusin'
-				var eventOut = trigger == 'hover' ? 'mouseleave' : 'focusout'
-				on(this.element, eventIn, this.enter.bind(this));
-				on(this.element, eventOut, this.leave.bind(this));
-			}
-		}
-		this.options.title = this.options.title || this.element.getAttribute('title');
-		this.element.removeAttribute('title');
-		if(this.options.delay && typeof this.options.delay == 'number') {
-			this.options.delay = {
-				show: this.options.delay,
-				hide: this.options.delay
-			}
-		};
 		//tip模板对应的dom
-		this.tipDom = makeDOM(this.options.template);
-		addClass(this.tipDom, this.options.placement);
-		if(this.options.colorLevel) {
-			addClass(this.tipDom, this.options.colorLevel);
+		this.tipDom = (0, _dom.makeDOM)(this.options.template);
+		(0, _dom.addClass)(this.tipDom, this.options.placement);
+		if (this.options.colorLevel) {
+			(0, _dom.addClass)(this.tipDom, this.options.colorLevel);
 		}
 		this.arrrow = this.tipDom.querySelector('.tooltip-arrow');
 
-		// tip容器,默认为当前元素的parent
-		this.container = this.options.container ? document.querySelector(this.options.container) : this.element.parentNode;
+		if(element&&element.length){
+			element.each(function(){
+				this.element = $(this)[0];
+				var triggers = oThis.options.trigger.split(' ');
+
+				for (var i = triggers.length; i--;) {
+					var trigger = triggers[i];
+					if (trigger == 'click') {
+						(0, _event.on)(this.element, 'click', oThis.toggle.bind(oThis,this.element));
+					} else if (trigger != 'manual') {
+						var eventIn = trigger == 'hover' ? 'mouseenter' : 'focusin';
+						var eventOut = trigger == 'hover' ? 'mouseleave' : 'focusout';
+						(0, _event.on)(this.element, eventIn, oThis.enter.bind(oThis,this.element));
+						(0, _event.on)(this.element, eventOut, oThis.leave.bind(oThis,this.element));
+					}
+				}
+				oThis.options.title = oThis.options.title || this.element.getAttribute('title');
+				this.element.removeAttribute('title');
+				if (oThis.options.delay && typeof oThis.options.delay == 'number') {
+					oThis.options.delay = {
+						show: oThis.options.delay,
+						hide: oThis.options.delay
+					};
+				};
+			});
+		}
+
 	},
-	enter: function() {
+	enter: function enter(element) {
 		var self = this;
+		//将tooltip中的element指定为其进入的当前element
+		this.element = element;
 		clearTimeout(this.timeout);
 		this.hoverState = 'in';
-		if(!this.options.delay || !this.options.delay.show) return this.show();
+		// tip容器,默认为当前元素的parent
+				this.container = this.options.container ? document.querySelector(this.options.container) : element.parentNode;
+		if (!this.options.delay || !this.options.delay.show) return this.show();
 
-		this.timeout = setTimeout(function() {
-			if(self.hoverState == 'in') self.show()
-		}, this.options.delay.show)
+		this.timeout = setTimeout(function () {
+			if (self.hoverState == 'in') self.show();
+		}, this.options.delay.show);
 	},
-	leave: function() {
+	leave: function leave() {
 		var self = this;
 		clearTimeout(this.timeout);
-		self.hoverState = 'out'
-		if(!self.options.delay || !self.options.delay.hide) return self.hide()
-		self.timeout = setTimeout(function() {
-			if(self.hoverState == 'out') self.hide()
-		}, self.options.delay.hide)
+		self.hoverState = 'out';
+		if (!self.options.delay || !self.options.delay.hide) return self.hide();
+		self.timeout = setTimeout(function () {
+			if (self.hoverState == 'out') self.hide();
+		}, self.options.delay.hide);
 	},
-	show: function() {
+	show: function show() {
 		var self = this;
 		this.tipDom.querySelector('.tooltip-inner').innerHTML = this.options.title;
-		this.tipDom.style.zIndex = getZIndex();
+		this.tipDom.style.zIndex = (0, _dom.getZIndex)();
 
-		if(this.options.showFix) {
+		if (this.options.showFix) {
 			document.body.appendChild(this.tipDom);
 			this.tipDom.style.position = 'fixed';
-			showPanelByEle({
+			(0, _dom.showPanelByEle)({
 				ele: this.element,
 				panel: this.tipDom,
 				position: "top"
 			});
 			// fix情况下滚动时隐藏
-			on(document, 'scroll', function() {
+			(0, _event.on)(document, 'scroll', function () {
 				self.hide();
-			})
+			});
 		} else {
 			this.container.appendChild(this.tipDom);
 			var inputLeft = this.element.offsetLeft;
@@ -110,9 +117,9 @@ Tooltip.prototype = {
 			var inputHeight = this.element.offsetHeight;
 			var topWidth = this.tipDom.offsetWidth;
 			var topHeight = this.tipDom.offsetHeight;
-			var tipDomleft,tipDomTop;
+			var tipDomleft, tipDomTop;
 
-			if(this.options.placement == 'top') {
+			if (this.options.placement == 'top') {
 				// 上部提示
 
 				this.left = this.element.offsetLeft + inputWidth / 2;
@@ -120,34 +127,34 @@ Tooltip.prototype = {
 				// 水平居中
 				tipDomleft = this.left - this.tipDom.clientWidth / 2 + 'px';
 				tipDomTop = this.top + 'px';
-			}else if (this.options.placement == 'bottom') {
+			} else if (this.options.placement == 'bottom') {
 				// 下边提示
 				this.left = this.element.offsetLeft + inputWidth / 2;
 				this.top = this.element.offsetTop + topHeight;
 				// 水平居中
 				tipDomleft = this.left - this.tipDom.clientWidth / 2 + 'px';
 				tipDomTop = this.top + 'px';
-			}else if (this.options.placement == 'left') {
+			} else if (this.options.placement == 'left') {
 				// 左边提示
-				this.left = this.element.offsetLeft ;
-				this.top = this.element.offsetTop + topHeight/2;
+				this.left = this.element.offsetLeft;
+				this.top = this.element.offsetTop + topHeight / 2;
 				tipDomleft = this.left - this.tipDom.clientWidth + 'px';
 
-				tipDomTop = this.top - this.tipDom.clientHeight/2 +'px';
-			}else {
+				tipDomTop = this.top - this.tipDom.clientHeight / 2 + 'px';
+			} else {
 				// 右边提示
-				
+
 				this.left = this.element.offsetLeft + inputWidth;
-				this.top = this.element.offsetTop + topHeight/2;
+				this.top = this.element.offsetTop + topHeight / 2;
 				tipDomleft = this.left + 'px';
-				tipDomTop = this.top - this.tipDom.clientHeight/2 +'px';
+				tipDomTop = this.top - this.tipDom.clientHeight / 2 + 'px';
 			}
-			
+
 			this.tipDom.style.left = tipDomleft;
 			this.tipDom.style.top = tipDomTop;
 		}
 
-		addClass(this.tipDom, 'active');
+		(0, _dom.addClass)(this.tipDom, 'active');
 
 		// var placement = this.options.placement;
 		// var pos = this.getPosition()
@@ -156,66 +163,62 @@ Tooltip.prototype = {
 		// var calculatedOffset = this.getCalculatedOffset(placement, pos, actualWidth, actualHeight)
 
 		// this.applyPlacement(calculatedOffset, placement)
-
 	},
-	hide: function() {
-		if(this.options.showFix) {
-			if(document.body.contains(this.tipDom)) {
-				removeClass(this.tipDom, 'active');
+	hide: function hide() {
+		if (this.options.showFix) {
+			if (document.body.contains(this.tipDom)) {
+				(0, _dom.removeClass)(this.tipDom, 'active');
 				document.body.removeChild(this.tipDom);
 			}
 		} else {
-			if(this.container.contains(this.tipDom)) {
-				removeClass(this.tipDom, 'active');
+			if (this.container.contains(this.tipDom)) {
+				(0, _dom.removeClass)(this.tipDom, 'active');
 				this.container.removeChild(this.tipDom);
 			}
 		}
-
 	},
-	applyPlacement: function(offset, placement) {
-		var width = this.tipDom.offsetWidth
-		var height = this.tipDom.offsetHeight
+	applyPlacement: function applyPlacement(offset, placement) {
+		var width = this.tipDom.offsetWidth;
+		var height = this.tipDom.offsetHeight;
 
 		// manually read margins because getBoundingClientRect includes difference
-		var marginTop = parseInt(this.tipDom.style.marginTop, 10)
-		var marginLeft = parseInt(this.tipDom.style.marginTop, 10)
+		var marginTop = parseInt(this.tipDom.style.marginTop, 10);
+		var marginLeft = parseInt(this.tipDom.style.marginTop, 10);
 
 		// we must check for NaN for ie 8/9
-		if(isNaN(marginTop)) marginTop = 0
-		if(isNaN(marginLeft)) marginLeft = 0
+		if (isNaN(marginTop)) marginTop = 0;
+		if (isNaN(marginLeft)) marginLeft = 0;
 
-		offset.top = offset.top + marginTop
-		offset.left = offset.left + marginLeft
+		offset.top = offset.top + marginTop;
+		offset.left = offset.left + marginLeft;
 
 		// $.fn.offset doesn't round pixel values
 		// so we use setOffset directly with our own function B-0
 		this.tipDom.style.left = offset.left + 'px';
 		this.tipDom.style.top = offset.top + 'px';
 
-		addClass(this.tipDom, 'active');
+		(0, _dom.addClass)(this.tipDom, 'active');
 
 		// check to see if placing tip in new offset caused the tip to resize itself
-		var actualWidth = this.tipDom.offsetWidth
-		var actualHeight = this.tipDom.offsetHeight
+		var actualWidth = this.tipDom.offsetWidth;
+		var actualHeight = this.tipDom.offsetHeight;
 
-		if(placement == 'top' && actualHeight != height) {
-			offset.top = offset.top + height - actualHeight
+		if (placement == 'top' && actualHeight != height) {
+			offset.top = offset.top + height - actualHeight;
 		}
-		var delta = this.getViewportAdjustedDelta(placement, offset, actualWidth, actualHeight)
+		var delta = this.getViewportAdjustedDelta(placement, offset, actualWidth, actualHeight);
 
-		if(delta.left) offset.left += delta.left
-		else offset.top += delta.top
+		if (delta.left) offset.left += delta.left;else offset.top += delta.top;
 
-		var isVertical = /top|bottom/.test(placement)
-		var arrowDelta = isVertical ? delta.left * 2 - width + actualWidth : delta.top * 2 - height + actualHeight
-		var arrowOffsetPosition = isVertical ? 'offsetWidth' : 'offsetHeight'
+		var isVertical = /top|bottom/.test(placement);
+		var arrowDelta = isVertical ? delta.left * 2 - width + actualWidth : delta.top * 2 - height + actualHeight;
+		var arrowOffsetPosition = isVertical ? 'offsetWidth' : 'offsetHeight';
 
 		//$tip.offset(offset)
 		this.tipDom.style.left = offset.left + 'px';
 		this.tipDom.style.top = offset.top - 4 + 'px';
 
 		// this.replaceArrow(arrowDelta, $tip[0][arrowOffsetPosition], isVertical)
-
 	},
 	getCalculatedOffset: function(placement, pos, actualWidth, actualHeight) {
 		return placement == 'bottom' ? {
