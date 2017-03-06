8d3691e72e1c0e33119967b857e551c6167bb3ba
LiuYueKai <liuyuekainihao@163.com>
2017-03-06 14:15:08 +0800

    docs: 增加新增组件说明文档

---
+++ docs/新增组件说明/自定义组件demo/vendor/uui/js/3.3.js
@@ -0,0 +1,21 @@
+webpackJsonp([3],{
+
+/***/ 153:
+/***/ function(module, exports) {
+
+	"use strict";
+
+	module.exports = function (module) {
+		if (!module.webpackPolyfill) {
+			module.deprecate = function () {};
+			module.paths = [];
+			// module.parent = undefined by default
+			module.children = [];
+			module.webpackPolyfill = 1;
+		}
+		return module;
+	};
+
+/***/ }
+
+});
